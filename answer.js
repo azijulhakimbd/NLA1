5d5a4accbc985a0e906ae40c2181b70a6becc728
azijulhakimbd <azijulhakim.sherpur@gmail.com>
2026-09-02 16:41:10 +0600

    solve:Q3 Answer

--- answer.js
+++ answer.js
@@ -19,3 +19,16 @@ function getDayType(day)
            return "Invalid Day"; 
     }
 }
+
+// Question 03
+function validateUsername(username) { 
+    if (username.length < 4) { 
+        return "Too Short"; 
+    } else if (username.includes(" ")) { 
+        return "No Space Allowed"; 
+    } else if (username.toLowerCase().includes("admin")) { 
+        return "Reserved Word"; 
+    } else { 
+        return "Available"; 
+    } 
+}
