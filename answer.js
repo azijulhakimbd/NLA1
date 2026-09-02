3c4471bbc6a1e6af449b7ec618568f26cb49f77a
azijulhakimbd <azijulhakim.sherpur@gmail.com>
2026-09-02 16:19:59 +0600

    solve:Q2 Answer

--- answer.js
+++ answer.js
@@ -2,3 +2,20 @@
 function describeValue(value) 
 { return `${typeof value} | ${value ? "truthy" : "falsy"}`; }
 
+// Question 02
+function getDayType(day)
+
+ { switch (day.toLowerCase()) 
+    {
+         case "friday": 
+         case "saturday": 
+           return "Weekend"; 
+
+         case "sunday": case "monday": 
+         case "tuesday": case "wednesday": 
+         case "thursday": 
+           return "Working Day"; 
+         default: 
+           return "Invalid Day"; 
+    }
+}
